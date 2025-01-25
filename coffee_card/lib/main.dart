7820cc952e40ba59a6b6542db3e0f42376c8e165
Arsel Morales <arsel.morales@gmail.com>
2025-01-25 23:47:43 +0800

    main copy, is the working camera functionlity

--- coffee_card/lib/main.dart
+++ coffee_card/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const PodScanApp());
@@ -29,7 +32,6 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    // Navigate to the home screen after 10 seconds
     Timer(const Duration(seconds: 7), () {
       Navigator.pushReplacement(
         context,
@@ -41,29 +43,28 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF832637), // Updated background color
+      backgroundColor: const Color(0xFF832637),
       body: Center(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Image.asset(
-              'assets/PODScan.png', // Updated logo path
-              height: 120, // Slightly smaller logo size
+              'assets/PODScan.png',
+              height: 120,
             ),
-            const SizedBox(width: 10), // Spacing between logo and text
-            // Vertical line container
+            const SizedBox(width: 10),
             Container(
-              height: 50, // Height same as the text
-              width: 2, // Width of the line
-              color: Colors.white, // Color of the vertical line
+              height: 50,
+              width: 2,
+              color: Colors.white,
             ),
-            const SizedBox(width: 10), // Spacing between line and text
+            const SizedBox(width: 10),
             RichText(
               text: const TextSpan(
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize: 36, // Slightly larger text size
+                  fontSize: 36,
                   fontWeight: FontWeight.normal,
                 ),
                 children: [
@@ -84,186 +85,118 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 }
 
-// Home Screen (after Splash Screen)
-class HomeScreen extends StatelessWidget {
+// Home Screen
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  File? _selectedImage;
+
+  Future<void> _pickImageFromGallery() async {
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _selectedImage = File(pickedFile.path);
+      });
+    }
+  }
+
+  Future<void> _takePhoto() async {
+    final ImagePicker picker = ImagePicker();
+    try {
+      final XFile? photo = await picker.pickImage(source: ImageSource.camera);
+
+      if (photo != null) {
+        setState(() {
+          _selectedImage = File(photo.path);
+        });
+      }
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
+
+  Future<void> _checkCameraPermission() async {
+    if (await Permission.camera.request().isGranted) {
+      await _takePhoto();
+    } else if (await Permission.camera.isPermanentlyDenied) {
+      await openAppSettings();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        // Set the background image
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/back.png'), // Update with your image path
-            fit: BoxFit.cover, // This will make the image cover the entire screen
+            image: AssetImage('assets/back.png'),
+            fit: BoxFit.cover,
           ),
         ),
         child: Center(
-          child: Stack(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              // Decorative corner borders (as you had them earlier)
-              Positioned(
-                top: 100,
-                left: 30,
-                child: Container(
-                  width: 70,
-                  height: 5,
+              Container(
+                width: 175,
+                height: 175,
+                decoration: BoxDecoration(
                   color: Colors.white,
+                  shape: BoxShape.circle,
                 ),
-              ),
-              Positioned(
-                top: 100,
-                left: 30,
-                child: Container(
-                  width: 5,
-                  height: 70,
-                  color: Colors.white,
-                ),
-              ),
-              Positioned(
-                top: 100,
-                right: 30,
-                child: Container(
-                  width: 70,
-                  height: 5,
-                  color: Colors.white,
-                ),
-              ),
-              Positioned(
-                top: 100,
-                right: 30,
-                child: Container(
-                  width: 5,
-                  height: 70,
-                  color: Colors.white,
-                ),
-              ),
-              Positioned(
-                bottom: 80,
-                left: 30,
-                child: Container(
-                  width: 70,
-                  height: 5,
-                  color: Colors.white,
-                ),
-              ),
-              Positioned(
-                bottom: 80,
-                left: 30,
-                child: Container(
-                  width: 5,
-                  height: 70,
-                  color: Colors.white,
-                ),
-              ),
-              Positioned(
-                bottom: 80,
-                right: 30,
-                child: Container(
-                  width: 70,
-                  height: 5,
-                  color: Colors.white,
-                ),
-              ),
-              Positioned(
-                bottom: 80,
-                right: 30,
-                child: Container(
-                  width: 5,
-                  height: 70,
-                  color: Colors.white,
-                ),
-              ),
-              // Main content
-              Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    // Logo - White Circle
-                    Container(
-                      width: 105, // Circle width
-                      height: 105, // Circle height
-                      decoration: BoxDecoration(
-                        color: Colors.white, // White color for the circle
-                        shape: BoxShape.circle, // Makes it a circle
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    // App name
-                    const Text.rich(
-                      TextSpan(
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 52,
-                          fontWeight: FontWeight.normal,
+                child: _selectedImage != null
+                    ? ClipOval(
+                        child: Image.file(
+                          _selectedImage!,
+                          fit: BoxFit.cover,
+                          width: 175,
+                          height: 175,
                         ),
-                        children: [
-                          TextSpan(
-                            text: 'Pod',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          TextSpan(text: 'Scan'),
-                        ],
+                      )
+                    : const Icon(
+                        Icons.camera_alt,
+                        size: 50,
+                        color: Colors.grey,
                       ),
-                    ),
-                    const SizedBox(height: 2),
-                    const Divider(
-                      color: Colors.white,
-                      thickness: 3,
-                      indent: 50,
-                      endIndent: 50,
-                    ),
-                    const SizedBox(height: 20),
-                    // Buttons
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFFF1E6BB), // Light yellow color
-                        foregroundColor: Colors.black, // Text color
-                        minimumSize: const Size(200, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25),
-                        ),
-                        elevation: 8, // Retains the shadow
-                        shadowColor: Colors.black.withOpacity(0.5), // Shadow color and opacity
-                      ),
-                      onPressed: () {
-                        // Add functionality for "Take Photo"
-                      },
-                      child: const Text(
-                        'Take Photo',
-                        style: TextStyle(
-                          // fontWeight: FontWeight.bold,  // Makes text bold
-                          fontSize: 20,  // Increases font size (adjust as needed)
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20), // Add space between the buttons
-
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF628E6E), // Updated to the #f1e6bb color
-                        foregroundColor: Colors.white, // Text color
-                        minimumSize: const Size(200, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25),
-                        ),
-                        elevation: 8, // Retains the shadow
-                        shadowColor: Colors.white.withOpacity(0.5), // Shadow color and opacity
-                      ),
-                      onPressed: () {
-                        // Add functionality for "Upload Photo"
-                      },
-                      child: const Text(
-                        'Upload Photo',
-                        style: TextStyle(
-                          //fontWeight: FontWeight.bold,  // Makes text bold
-                          fontSize: 20,  // Increases font size (adjust as needed)
-                        ),
-                      ),
-                    ),
-
-                  ],
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFF1E6BB),
+                  foregroundColor: Colors.black,
+                  minimumSize: const Size(200, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  elevation: 8,
+                ),
+                onPressed: _checkCameraPermission,
+                child: const Text(
+                  'Take Photo',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF628E6E),
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(200, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  elevation: 8,
+                ),
+                onPressed: _pickImageFromGallery,
+                child: const Text(
+                  'Upload Photo',
+                  style: TextStyle(fontSize: 20),
                 ),
               ),
             ],
